107aad63b93e82a33cbca7ff4967e304be480689
Sky Luo <112263717+skyyylaw@users.noreply.github.com>
2026-07-31 18:47:33 -0400

    Add: rotting-fruit - submission-9

---
+++ Data Structures & Algorithms/rotting-fruit/submission-9.py
@@ -0,0 +1,56 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        neighborsOf = defaultdict(list)
+        rows = len(grid)
+        cols = len(grid[0])
+
+        nextRottingFruits = []
+        fresh = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    nextRottingFruits.append((r, c))
+                elif grid[r][c] == 1:
+                    fresh += 1
+
+        if fresh == 0:
+            return 0
+
+        rottedFresh = set()
+        temp = []
+        
+        time = -1
+        while nextRottingFruits:
+            r,c = nextRottingFruits.pop()
+            
+            if grid[r][c] == 1:
+                rottedFresh.add((r,c))
+
+            # this fresh fruit turns bad in this round
+            grid[r][c] = 2
+
+            # add all neighboring freshfruits for next round
+            if r-1 >= 0 and grid[r-1][c] == 1:
+                temp.append((r-1, c))
+            if r+1 < rows and grid[r+1][c] == 1:
+                temp.append((r+1, c))
+            if c-1 >= 0 and grid[r][c-1] == 1:
+                temp.append((r, c-1))
+            if c+1 < cols and grid[r][c+1] == 1:
+                temp.append((r, c+1))
+            
+            if not nextRottingFruits:
+                time += 1
+                for r, c in temp:
+                    if grid[r][c] == 1:
+                        nextRottingFruits.append((r,c))
+                temp = []
+
+        return time if len(rottedFresh) == fresh else -1
+
+                
+
+
+
+
+
